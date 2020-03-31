b7a1469f4910992220d3cd9581d9fa39e5174666
win_yohan <12dhdygks@naver.com>
2020-03-31 20:30:22 +0900

    QtableView 관련 기능 개선
    - entry 클릭 시 파일 및 디렉토리 실행되도록
    - 초기 루트 디렉토리에서 파일 및 디렉토리 수집하지 않는 현상 해결
    - 파일의 사이즈 정보가 올바르게 표시되지 않는 문제 해결
    - 정렬 기능 구현

--- UI.py
+++ UI.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QTableWidget, QLineEdit, QVBoxLayout, QTableWidgetItem, QLabel, QTableView
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QVBoxLayout, QLabel, QTableView
 from PyQt5.QtCore import *
 from DBManager import DBManager
 from UIthread import ScanThread, ReadDBThread
@@ -10,72 +10,67 @@ import pandas as pd
 
 #from UI_handler import *
 
-
+########################################################################################################################
+# UI 클래스
+# UI 를 구성하기 위한 클래스
+# UI 뿐만 아니라 각 UI 위젯 간의 시그널을 관리하기 위한 시그널 핸들러 함수도 정의하고 있다.
+########################################################################################################################
 class UI(QWidget):
     def __init__(self):
         super().__init__()
         self.DB = DBManager()
 
+        # DB 정보를 읽기 위한 쓰레드
         self.read_db_thread = ReadDBThread()
         self.read_db_thread.set_db(self.DB)
         self.read_db_thread.finish_read_signal.connect(self.displayFiles)
 
+        # 디렉토리를 탐색해서 파일 리스트를 얻은 후, DB에 insert 하는 작업까지 수행하는 쓰레드
         self.scan_thread = ScanThread()
         self.scan_thread.set_db(self.DB)
         self.scan_thread.finish_scan_signal.connect(self.finish_scan)
-        #self.start_scan_thread()
+        self.start_scan_thread()
 
         self.initUI()
 
-    """
-    def setTable(self):
-        column_headers = ['파일 이름', '파일 경로', '크기(KB)']
-        self.tableWidget.setHorizontalHeaderLabels(column_headers)
-        self.tableWidget.setColumnCount(3)
-        self.header_sorted_state = [False, False, False]
-
-        self.tableWidget.cellDoubleClicked.connect(self.execute_file)
-        self.tableWidget.horizontalHeader().sectionClicked.connect(self.sort_data)
-
-        self.start_read_db_thread(all_data = True)
-
+    ####################################################################################################################
+    # setTableData
+    # - file_list : DB로 SQL 쿼리문을 통해 획득한, 테이블에 표시할 파일 정보(이름, 경로, 사이즈) 가 저장되어 있는 리스트
+    # = file_list를 넘겨 받아 몇 개에 파일을 넘겨 받았는지를 label 위젯에 표시한다.
+    #   데이터를 표시하는 tableview 에 기존 정보가 포함되어 있을 수 있으므로 clear 해준 후, 데이터를 표시한다.
+    ####################################################################################################################
     def setTableData(self, file_list):
-        num_of_rows = len(file_list)
-        self.tableWidget.setRowCount(num_of_rows)
-        self.label.setText(str(num_of_rows) + " 개 검색됨")
-        row = 0
-
-        for file in file_list:
-            name = file[0]
-            path = file[1]
-            size = str(file[2])
-
-            self.tableWidget.setItem(row, 0, QTableWidgetItem(name))
-            self.tableWidget.setItem(row, 1, QTableWidgetItem(path))
-            self.tableWidget.setItem(row, 2, QTableWidgetItem(size))
-            row = row + 1
-            #if row == 20 :
-             #   break
-
-        self.tableWidget.resizeColumnsToContents()
-        self.tableWidget.horizontalHeader().setStretchLastSection(True)
-
-    """
-
-    def setTableData(self, data):
-        total = len(data)
+        total = len(file_list)
         self.label.setText("총 {0} 개 검색됨".format(total))
-        self.table_model.setData(data)
+        self.tableview.clearSpans()
+
+        self.table_model.setDataFrame(file_list)
         self.tableview.setModel(self.table_model)
         self.tableview.resizeColumnsToContents()
 
-
+    ####################################################################################################################
+    # setTable
+    # = 테이블 표시를 위한 기본 설정을 하는 메소드이다.
+    #   테이블의 entry를 클릭 했을 때, 파일이나 디렉토리를 실행하기 위한 이벤트(execute_file)를 연결해주고,
+    #   테이블의 헤더를 클릭 했을 때 정렬 기능을 실행하기 위한 이벤트(sort_data)도 연결해준다.
+    ####################################################################################################################
     def setTable(self):
+        self.header_sorted_state = [False, False, False]
         self.table_model = TableModel()
-        self.start_read_db_thread(all_data=True)
-        self.tableview.doubleClicked.connect(self.execute_file)
 
-    # 사용자 UI 초기화를 담당하는 메소드
+        self.tableview.setSortingEnabled(True)
+        self.start_read_db_thread(all_data=True)
+
+        self.tableview.doubleClicked.connect(self.execute_file)
+        self.tableview.horizontalHeader().sectionClicked.connect(self.sort_data)
+
+
+    ####################################################################################################################
+    # inintUI
+    # = UI를 초기화하기 위한 메소드이다.
+    #   사용자 입력을 받기 위한 LineEdit, 검색된 결과 개수를 표시하기 위한 Label, 검색된 결과를 표시하기 위한 tableview
+    #   위젯들을 포함한다.
+    ####################################################################################################################
     def initUI(self):
 
         # 사용자 입력을 받는 위젯
@@ -85,8 +80,7 @@ class UI(QWidget):
         # 검색된 결과의 개수를 표시하는 위젯
         self.label = QLabel(self)
 
-        # 검색된 결과를 표시하는 위젯
-        #self.tableWidget = QTableWidget(self)
+        # 검색된 결과를 표시하는 테이블
         self.tableview = QTableView()
         self.setTable()
 
@@ -101,35 +95,48 @@ class UI(QWidget):
         self.setGeometry(800, 200, 1500, 1000)
         self.show()
 
+    ####################################################################################################################
+    # 이벤트 핸들러 메소드
+    ####################################################################################################################
+    @pyqtSlot(int)
+    def sort_data(self, column_index):
+        print("sort")
+        s = time.time()
+        self.table_model.layoutAboutToBeChanged.emit()
+        if self.header_sorted_state[column_index]:
+            self.table_model._data = self.table_model._data.sort_values(self.table_model.headers[column_index])
+            self.header_sorted_state[column_index] = False
+        else:
+            self.table_model._data = self.table_model._data.sort_values(self.table_model.headers[column_index], ascending=False)
+            self.header_sorted_state[column_index] = True
+        e = time.time()
+        print("sorting time : ", e - s)
+
+        self.table_model.layoutChanged.emit()
 
     # 숫자 정렬 구현하기... 숫자도 문자로 인식해서 정렬중..
-    @pyqtSlot(int)
-    def sort_data(self, logicalIndex):
-        s = time.time()
-        if (self.header_sorted_state[logicalIndex]):
-            self.tableview.sortItems(logicalIndex, Qt.AscendingOrder)
-            self.header_sorted_state[logicalIndex] = False
-        else:
-            self.tableview.sortItems(logicalIndex, Qt.DescendingOrder)
-            self.header_sorted_state[logicalIndex] = True
-        e = time.time()
-        print(e - s)
+    @pyqtSlot(QModelIndex)
+    def execute_file(self, signal):
+        row = signal.row()
 
-    @pyqtSlot()
-    def execute_file(self):
-        current_row = self.tableview.selectionModel().currentIndex().row()
-        path = self.tableview.index(current_row, 1)
-        print(path)
+        index = signal.sibling(row, 1)
+        index_dict = self.table_model.itemData(index)
+        path = index_dict.get(0)
+
         webbrowser.open(path)
 
     @pyqtSlot(bool)
     def finish_scan(self, check):
-        print("finish scan")
+        print("scan completed!!")
         pass
+
 
     @pyqtSlot(list)
     def displayFiles(self, file_list):
-        self.setTableData(file_list)
+        try:
+            self.setTableData(file_list)
+        except Exception as e:
+            print("dis : ", e)
 
     # 사용자의 입력값이 변경될 때 실행되는 메소드
     @pyqtSlot()
@@ -152,22 +159,29 @@ class UI(QWidget):
 class TableModel(QAbstractTableModel):
     def __init__(self):
         super(TableModel, self).__init__()
+        self.headers = ["File", "Path", "Size(KB)"]
 
-    def setData(self, data):
-        self._data = data
+    def setDataFrame(self, filelist):
+        self._data = pd.DataFrame(filelist, columns=self.headers)
 
     def data(self, index, role):
         if role == Qt.DisplayRole:
-            return self._data[index.row()][index.column()]
+            row = index.row()
+            col = index.column()
+            value = self._data.iloc[row, col]
 
-    def rowCount(self, index):
-        return len(self._data)
+            return QVariant(str(value))
 
-    def columnCount(self, index):
-        if len(self._data) == 0:
-            return 0
+    def rowCount(self, parent=None):
+        return len(self._data.index)
 
-        return len(self._data[0])
+    def columnCount(self, parent=None):
+        return len(self._data.columns.values)
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return str(self._data.columns[section])
 
 
 if __name__ == '__main__':
