a4eb0dcdf49a09bb42daaef7c3943e36e35b7938
win_yohan <12dhdygks@naver.com>
2020-04-07 21:21:01 +0900

    기능 수정
    - 드라이브 별로 스캔을 수행하도록 멀티 프로세스 구성
    - 파일 변경(생성)을 감지해서 시그널로 UI에게 전달하도록 구현

--- UIthread.py
+++ UIthread.py
@@ -1,13 +1,17 @@
 from PyQt5.QtCore import QThread, pyqtSignal, QMutex, pyqtSlot
 import os
-from multiprocessing import Manager, freeze_support, Pool, Process
+import re
+from multiprocessing import Manager, freeze_support, Pool, Process, Pipe, Queue
 from itertools import repeat
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 import time
 from time import sleep
 import math
 
-# 스택에 탐색 대상 디렉토리를 넣고 탐색하는 방법도 고려..
+
 # 권한 상승 코드 고려 필요
 ########################################################################################################################
 # make_filelist
@@ -17,35 +21,21 @@ import math
 # = 사용자로부터 입력 받은 세 가지의 인자를 하나의 리스트에 넣어주는 메소드
 ########################################################################################################################
 def make_filelist(content, path, isdir):
-    temp = []
-
-    temp.append(content)
-    temp.append(path)
-
+    
     if isdir:
         size = 0
     else:
-        size = math.ceil(os.stat(os.path.join(path, content)).st_size / 1024)
+        try:
+            size = math.ceil(os.stat(os.path.join(path, content)).st_size / 1024)
+        except FileNotFoundError:
+            return
+        except PermissionError:
+            return
 
-    temp.append(size)
+    temp = (content, path, size)
     return temp
 
-
-# 가장 최신
-########################################################################################################################
-# init_filelist
-# = 사용자의 cpu 수 * 2의 갯수에 해당하는 프로세스를 생성하고, 각 프로세스들이 서로 다른 경로의 디렉토리를 탐색해서
-#   하위 파일 및 디렉토리 정보를 수집하도록 하는 함수
-########################################################################################################################
-def init_filelist():
-    root_dir = "c:\\"
-    result_list = search(root_dir)
-
-    return result_list
-
-
-# target_dir : 각 디스크 root_dir
-def search(target_dir):
+def search(target_dir, result_queue):
     result_list = []
     re_app = result_list.append
 
@@ -53,59 +43,57 @@ def search(target_dir):
     re_app(temp)
 
     try:
+        s = time.time()
         # 루트 디렉토리에서 탐색하여 모든 파일 및 디렉토리 목록 스캔
         for path, dirs, files in os.walk(target_dir):
             if len(dirs) > 0:
                 for d in dirs:
                     temp = make_filelist(d, path, True)
-                    re_app(temp)
 
             for fname2 in files:
                 temp = make_filelist(fname2, path, False)
-                re_app(temp)
 
-    except WindowsError as e:
-        #print("win : ", e)
+        e = time.time()
+        print(target_dir, "os.walk : ", e - s)
+    except PermissionError as e:
         pass
-    except OSError as e:
-        #print("os : ", e)
-        pass
-    except Exception as e:
-        #print("exception : ", e)
-        pass
+    
+    result_queue.put(result_list)
 
-    return result_list
-
-"""
 ########################################################################################################################
 # init_filelist
 # = 사용자의 cpu 수 * 2의 갯수에 해당하는 프로세스를 생성하고, 각 프로세스들이 서로 다른 경로의 디렉토리를 탐색해서
 #   하위 파일 및 디렉토리 정보를 수집하도록 하는 함수
 ########################################################################################################################
-def init_filelist():
-    root_dir = "c:\\"
-    files = os.listdir(root_dir)
-    filelist = []
-
-    for file in files:
-        filelist.append(os.path.join(root_dir, file))
-
-    cpu = os.cpu_count()
-    pool = Pool(cpu * 2)
+def init_filelist(drive_dirs):
+    freeze_support()
 
     manager = Manager()
-    result_list = manager.list()
+    result_queue = manager.Queue()
+    
+    result_list = []
+    jobs = []
 
+    drive_dirs.reverse()
     try:
-        pool.starmap(search, zip(filelist, repeat(result_list)))
+        for drive in drive_dirs:
+            p = Process(target = search, args = (drive, result_queue, ))
+            jobs.append(p)
+            p.start()         
     finally:
-        pool.close()
-        pool.join()
+        for proc in jobs:
+            proc.join()
 
+    while result_queue.qsize() > 0:
+        result_list.extend(result_queue.get())
+    
     return result_list
-"""
+
+
 class CommonThread(QThread):
     running = QMutex()
+    drive_dirs = re.findall(r"[A-Z]+:.*$",os.popen("mountvol /").read(),re.MULTILINE)
+    
 
     def __init__(self):
         super().__init__()
@@ -120,7 +108,7 @@ class CommonThread(QThread):
 ########################################################################################################################
 class ScanThread(CommonThread):
 
-    finish_scan_signal = pyqtSignal()
+    finish_scan_signal = pyqtSignal(list)
     
     def __init__(self):
         super().__init__()
@@ -128,10 +116,9 @@ class ScanThread(CommonThread):
     def run(self):
         print("run..")
         s = time.time()
-        file_list = init_filelist()
+        file_list = init_filelist(self.drive_dirs)
         e = time.time()
-
-        print(len(file_list))
+        self.finish_scan_signal.emit(file_list)
         print("scan time : ", e - s)
 
         s = time.time()
@@ -141,7 +128,6 @@ class ScanThread(CommonThread):
         e = time.time()
 
         print("insert time : ", e - s)
-        self.finish_scan_signal.emit()
     
     
 ########################################################################################################################
@@ -149,22 +135,117 @@ class ScanThread(CommonThread):
 # 사용자로부터 입력 받은 문자열을 통해, 해당 문자열을 포함하는 파일명을 가진 파일의 정보를 DB로부터 넘겨받아 처리하는 클래스
 ########################################################################################################################
 class ReadDBThread(CommonThread):
-
     finish_read_signal = pyqtSignal(list)
-    
+
     def __init__(self):
         super().__init__()
-
-    def set_finding_str(self, finding_str):
-        self.finding_str = finding_str
     
-    def run(self, all_data = False):
+    def run(self):
         self.running.lock()
-        if all_data: # 초기 테이블 표시를 위해
-            result = self.db.get_all_filelist()
-        else:
-            result = self.db.get_filelist_by_file_name(self.finding_str)
-
+        result = self.db.get_all_filelist()
         self.running.unlock()
-
         self.finish_read_signal.emit(result)
+
+class ManagerObserverThread(CommonThread):
+    flag = QMutex()
+    file_info = []
+    detect_create_signal = pyqtSignal(tuple)
+
+    def __init__(self):
+        super().__init__()
+        self.observer_threads = []
+
+    def run(self):
+        index = 0
+        for drive in self.drive_dirs:
+            self.observer_threads.append(ObserverThread())
+            self.observer_threads[index].set_watchDir(drive)
+            index = index + 1
+
+        for observer in self.observer_threads:
+            try:
+                observer.start()
+            except PermissionError:
+                pass
+        
+        self.check_file_info()
+
+
+    def check_file_info(self):
+        while True:
+            self.flag.lock()
+            while len(self.file_info) > 0:
+                print("check : ", self.file_info[0])
+                self.detect_create_signal.emit(self.file_info.pop())
+            self.flag.unlock()
+        
+
+class ObserverThread(CommonThread):
+
+    def __init__(self):
+        super().__init__()
+        self.observer = Observer()
+
+    def set_watchDir(self, watchDir):
+        self.watchDir = watchDir
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.watchDir, recursive=True)
+        
+        try:
+            self.observer.start()
+        except PermissionError:
+            return
+
+        try:
+            while True:
+                time.sleep(60)
+        except PermissionError as e:
+            pass
+
+        except Exception as e:
+            self.observer.stop()
+            self.observer.join()
+
+class Handler(FileSystemEventHandler, ManagerObserverThread):
+#FileSystemEventHandler 클래스를 상속받음.
+#아래 핸들러들을 오버라이드 함
+
+    #파일, 디렉터리가 move 되거나 rename 되면 실행
+    def on_moved(self, event):
+        full_path = event.src_path
+        file_name = os.path.basename(full_path)
+        file_dir = os.path.dirname(full_path)
+        
+        temp = make_filelist(file_name, file_dir, os.path.isdir(full_path))
+
+
+    def on_created(self, event): #파일, 디렉터리가 생성되면 실행 -> 추가
+        full_path = event.src_path
+        file_name = os.path.basename(full_path)
+        file_dir = os.path.dirname(full_path)
+
+        temp = make_filelist(file_name, file_dir, os.path.isdir(full_path))
+        if temp is None:
+            return
+
+        self.flag.lock()
+        self.file_info.append(temp)
+        self.flag.unlock()
+
+
+    def on_deleted(self, event): #파일, 디렉터리가 삭제되면 실행
+        full_path = event.src_path
+        file_name = os.path.basename(full_path)
+        file_dir = os.path.dirname(full_path)
+
+        temp = make_filelist(file_name, file_dir, os.path.isdir(full_path))
+
+
+    def on_modified(self, event): #파일, 디렉터리가 수정되면 실행
+        full_path = event.src_path
+        file_name = os.path.basename(full_path)
+        file_dir = os.path.dirname(full_path)
+
+        temp = make_filelist(file_name, file_dir, os.path.isdir(full_path))
